d592e2164f8de65f1e716d1f4506b0f834fa2ab1
Polina Pavlova <poli.paskaleva@gmail.com>
2020-12-15 13:43:59 +0200

    Merge remote-tracking branch 'origin/master' into master

--- FundamentalsFinalExam/src/Problem1.java
+++ FundamentalsFinalExam/src/Problem1.java
@@ -8,14 +8,14 @@ class Problem1 {
     public static
     void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-        String email = scanner.nextLine ();
+        String  email   = scanner.nextLine ();
 
         String command = scanner.nextLine ();
 
-        while (!"Complete".equals (command)){
+        while (!"Complete".equals (command)) {
             String[] tokens = command.split ("\\s+");
 
-            switch (tokens[0]){
+            switch (tokens[0]) {
                 case "Make":
                     switch (tokens[1]) {
                         case "Upper" -> {
@@ -30,27 +30,27 @@ class Problem1 {
                     break;
                 case "GetDomain":
                     int count = Integer.parseInt (tokens[1]);
-                    System.out.println (email.substring (email.length ()-count));
+                    System.out.println (email.substring (email.length () - count));
                     break;
                 case "GetUsername":
-                    if (email.contains ("@")){
+                    if (email.contains ("@")) {
                         int indexOfEt = email.indexOf ('@');
-                        System.out.println (email.substring (0,indexOfEt));
-                    }else {
-                        System.out.printf ("The email %s doesn't contain the @ symbol.%n",email);
+                        System.out.println (email.substring (0, indexOfEt));
+                    } else {
+                        System.out.printf ("The email %s doesn't contain the @ symbol.%n", email);
                     }
                     break;
                 case "Replace":
                     String symbol = tokens[1];
-                  email = email.replace (symbol,"-");
+                    email = email.replace (symbol, "-");
                     System.out.println (email);
                     break;
                 case "Encrypt":
                     List<Integer> numbers = new ArrayList<> ();
                     for (char c : email.toCharArray ()) {
-                        numbers.add ((int)c);
+                        numbers.add ((int) c);
                     }
-                    System.out.println (numbers.toString ().replaceAll ("[\\[\\],]",""));
+                    System.out.println (numbers.toString ().replaceAll ("[\\[\\],]", ""));
 
                     break;
 
